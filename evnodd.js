48bbf510424360ccb9da9c397425235148109a3c
TheSigma1 <97096942+TheSigma1@users.noreply.github.com>
2022-01-04 18:02:38 +0530

    Add files via upload

---
+++ evnodd.js
@@ -0,0 +1,23 @@
+var G = -22
+function oddeven ()
+
+{
+if(G%2==0)
+{
+    if(G>0)
+  console.log("its even and positive");
+  else{
+      console.log("its even and negative");
+  }
+}
+else
+{
+  if (G>0) {
+    console.log("its odd and positive");
+}
+else{
+    console.log("its odd and negative")
+    }
+  } 
+}
+oddeven()
